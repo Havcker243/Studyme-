c3518b7acd6c1bff95284dec93dae07b185e0134
Oludolapo Adegbesan <oadegbesan25@my.whitworth.edu>
2025-01-24 17:33:16 -0600

    update to the route

--- App/routes.py
+++ App/routes.py
@@ -1,1 +1,13 @@
-"test"
+from flask import Blueprint
+#Blueprint( this is used to organize all the large files and endpoints used in the project )
+
+main = Blueprint("main", __name__)
+
+#The main endpoint for the program
+@main.route('/')
+def home():
+    return "Starting the studyme bot "
+
+@main.route('/summarize', method = ['POST'])
+def summarize():
+    
